a93e065879612e5064ff352336fe87b5be1c787f
John Källström <john@slasktratt.se>
2014-01-30 16:13:27 +0100

    Only inject Jrebel-stuff if it's acutally there

--- provision/modules/tomcat7/files/jrebel/jrebel-tomcat.sh
+++ provision/modules/tomcat7/files/jrebel/jrebel-tomcat.sh
@@ -2,4 +2,8 @@
 
 # Managed by puppet :-)
 
-#export JAVA_OPTS="-javaagent:/opt/tomcat7/jrebel/jrebel.jar $JAVA_OPTS"
+# Inject JRebel-stuff only if jrebel.jar is not empty.
+if [ -s /opt/tomcat7/jrebel/jrebel.jar ]
+then
+	export JAVA_OPTS="-javaagent:/opt/tomcat7/jrebel/jrebel.jar $JAVA_OPTS"
+fi

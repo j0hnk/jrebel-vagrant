a97f5fe094346097cd929749f4ea2e4c02d5ed52
John Källström <john@slasktratt.se>
2014-02-02 10:59:01 +0100

    cleanup

--- provision/modules/tomcat7/files/jrebel/jrebel-tomcat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Managed by puppet :-)
-
-# Inject JRebel-stuff only if jrebel.jar is not empty.
-if [ -s /opt/tomcat7/jrebel/jrebel.jar ]
-then
-	export JAVA_OPTS="-javaagent:/opt/tomcat7/jrebel/jrebel.jar $JAVA_OPTS"
-fi
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++
@@ -1,6 +0,0 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c" %>
-<html>
-<body>
-<h2>Hello World from ${host}</h2>
-</body>
-</html>
